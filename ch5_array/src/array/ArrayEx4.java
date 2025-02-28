cd08be4fad240dc0c88b32b5241bbdc1dc70f96f
SinSeonHo <loop63@naver.com>
2025-02-28 17:41:58 +0900

    배열 예제

--- ch5_array/src/array/ArrayEx4.java
+++ ch5_array/src/array/ArrayEx4.java
@@ -13,11 +13,28 @@ public class ArrayEx4 {
         Scanner sc = new Scanner(System.in);
 
         // n명 학생의 수학점수를 입력받아서 평균, 총합 구하기
+        // System.out.print("학생의 수 입력 >> ");
+        // int num = sc.nextInt();
+
+        // int[] arr = new int[num]; // 배열 선언 및 입력받은 수만큼 생성
+        // int sum = 0;
+
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.print(i + 1 + "번째 학생 점수 입력 >> ");
+        // arr[i] = sc.nextInt();
+        // sum += arr[i];
+        // }
+
+        // System.out.println("현재 입력된 점수 : " + Arrays.toString(arr));
+
+        // System.out.printf("평균 : %.2f점, 총합 : %d점", (double) sum / arr.length, sum);
+
         System.out.print("학생의 수 입력 >> ");
         int num = sc.nextInt();
 
         int[] arr = new int[num]; // 배열 선언 및 입력받은 수만큼 생성
         int sum = 0;
+        double avg = 0;
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(i + 1 + "번째 학생 점수 입력 >> ");
@@ -25,9 +42,11 @@ public class ArrayEx4 {
             sum += arr[i];
         }
 
+        avg = (double) sum / arr.length;
+
         System.out.println("현재 입력된 점수 : " + Arrays.toString(arr));
 
-        System.out.printf("평균 : %.2f점, 총합 : %d점", (double) sum / arr.length, sum);
+        System.out.printf("평균 : %.2f점, 총합 : %d점", avg, sum);
 
     }
 }
